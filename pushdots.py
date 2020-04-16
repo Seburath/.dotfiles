dc028c662fee0f725c4c0eed33311199f4c0ebb3
Sebastian <Seburath@gmail.com>
2020-04-15 23:24:21 -0500

    Delete pushdots.py

--- pushdots.py
+++
@@ -1,8 +0,0 @@
-import os
-from shutil import copyfile
-
-username = os.getcwd().split('/')[2]
-path = '/home/{}/'.format(username)
-
-copyfile(path + '.zshrc', 'zshrc')
-copyfile(path + '.vimrc', 'vimrc')
