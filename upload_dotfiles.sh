c7f5c71ae3d8cc7c8d405958ee0b62bb592417be
Seburath <seburath@gmail.com>
2020-06-13 18:47:00 -0500

    dotfiles update

---
+++ upload_dotfiles.sh
@@ -0,0 +1,10 @@
+pacman -Qqen > pacman-pkglist.txt
+yay -Qqen > yay-pkglist.txt
+cp ~/.zshrc ./.zshrc
+cp ~/.vimrc ./.vimrc
+cp -r ~/.config ./.config
+
+sudo git add *
+git commit -m 'dotfiles update'
+git push
+#pacman -S - < pkglist.txt
