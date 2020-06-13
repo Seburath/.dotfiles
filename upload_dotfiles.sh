6c9dc50023d045e9890becbe0f69363bacbebbcc
Seburath <seburath@gmail.com>
2020-06-13 18:59:34 -0500

    dotfiles update

--- upload_dotfiles.sh
+++ upload_dotfiles.sh
@@ -1,8 +1,8 @@
 pacman -Qqen > pacman-pkglist.txt
 yay -Qqen > yay-pkglist.txt
-cp ~/.zshrc ./.zshrc
-cp ~/.vimrc ./.vimrc
-cp -r ~/.config ./.config
+cp ~/.zshrc .zshrc
+cp ~/.vimrc .vimrc
+cp -r ~/.config .config
 
 sudo git add *
 git commit -m 'dotfiles update'
