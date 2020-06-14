8103ecfdd517a433e0cb3ac6ae26c863281fd085
Seburath <seburath@gmail.com>
2020-06-13 19:20:21 -0500

    dotfiles updated

--- .config/cmus/lib.pl
+++
@@ -1,2 +0,0 @@
-/home/sebu/Downloads/y2mate.com - The Glitch Mob - Drink the Sea (10 Year Anniversary Full Album Visual)_ezk_dD2Ia-w.mp3
-/home/sebu/Downloads/y2mate.com - The Glitch Mob - Love Death Immortality (Full Album)_LBZ-3Ugj1AQ.mp3
--- upload_dotfiles.sh
+++ upload_dotfiles.sh
@@ -1,10 +1,11 @@
 pacman -Qqen > pacman-pkglist.txt
 yay -Qqen > yay-pkglist.txt
+
 cp ~/.zshrc .zshrc
 cp ~/.vimrc .vimrc
+cp ~/.oh-my-zsh/themes/intheloop.zsh-theme fire.zsh-theme
 
-cp -r ~/.config/ .config
-cp ~/.oh-my-zsh/themes/intheloop.zsh-theme oh-my-zsh .oh-my-zsh/themes/intheloop.zsh-theme
+cp -r ~/.config/terminator .config/terminator
 
 sudo git add *
 git commit -m 'dotfiles update'
