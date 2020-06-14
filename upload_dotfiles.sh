4cd90be4cfd3e0a544bfe61c2742667c59436282
Seburath <seburath@gmail.com>
2020-06-13 19:54:21 -0500

    dotfiles update

--- upload_dotfiles.sh
+++ upload_dotfiles.sh
@@ -5,7 +5,9 @@ cp ~/.zshrc .zshrc
 cp ~/.vimrc .vimrc
 cp ~/.oh-my-zsh/themes/intheloop.zsh-theme fire.zsh-theme
 
-cp -r ~/.config/terminator .config/terminator
+cp -r ~/.config/terminator/* .config/terminator/*
+
+sudo chmod -R ugo+rwX .
 
 sudo git add *
 git commit -m 'dotfiles update'
